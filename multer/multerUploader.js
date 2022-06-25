6b104fe045fbc41abd3ec3ba43ebfc16c8dbb9b1
Akrom <akobidov777@gmail.com>
2022-06-25 14:57:15 +0500

    multer

---
+++ multer/multerUploader.js
@@ -0,0 +1,86 @@
+const multer = require("multer");
+const path = require("path")
+
+const singleMulter = multer({
+    storage: multer.memoryStorage(),
+    limits: {
+        fileSize: 10000000
+    },
+    fileFilter: function (req, file, cb) {
+        checkFileType(file, cb);
+    }
+}).single("upload");
+
+const multiMulter = multer({
+    storage: multer.memoryStorage(),
+    limits: {
+        fileSize: 10000000
+    },
+    fileFilter: function (req, file, cb) {
+        checkFileType(file, cb);
+    }
+}).array("upload", 6);
+function checkFileType(file, cb) {
+    // Allowed ext
+    const filetypes = /jpeg|jpg|png|gif/;
+    // Check ext
+    const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+    // Check mime
+    // const mimetype = filetypes.test(file.mimetype);
+    if (extname) {
+        return cb(null, true);
+    } else {
+        return cb("E`lon joylashda muammo bor. Qoidalarga ko'ra .jpeg, .jpg, .png, .gif turidagi va 10 MB gacha rasmlarni joylashingiz mumkin.");
+    }
+}
+
+const uploadAvatar =  (req, res, next) => {
+    singleMulter(req, res, function (err) {
+        if (err instanceof multer.MulterError) {
+            // A Multer error occurred when uploading.
+            console.log("error in multer")
+            return res.status(400).json({
+                error: err,
+                errorCode: "1",
+                message: "Yuklashda xatolik sodir bo'ldi"
+            })
+        } else if (err) {
+            // An unknown error occurred when uploading.
+            console.log("error in uploading, ", err)
+            return res.status(400).json({
+                error: err,
+                errorCode: "1",
+                message: "Yuklashda xatolik sodir bo'ldi"
+            })
+        }
+        // Everything went fine. 
+        next()
+    })
+}
+const uploadMultiImages = (req,res, next)=>{
+    multiMulter(req, res, function (err) {
+        if (err instanceof multer.MulterError) {
+            // A Multer error occurred when uploading.
+            console.log("error in multer")
+            return res.status(400).json({
+                error: err,
+                errorCode: "1",
+                message: "Yuklashda xatolik sodir bo'ldi"
+            })
+        } else if (err) {
+            // An unknown error occurred when uploading.
+            console.log("error in uploading, ", err)
+            return res.status(400).json({
+                error: err,
+                errorCode: "1",
+                message: "Yuklashda xatolik sodir bo'ldi"
+            })
+        }
+        // Everything went fine. 
+        next()
+    })
+}
+
+module.exports = {
+    uploadAvatar, uploadMultiImages
+}
