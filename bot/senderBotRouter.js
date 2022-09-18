5d87f86dbcd298350062b57a055fe61b08950346
Akrom <akobidov777@gmail.com>
2022-09-18 21:34:18 +0500

    telegram bot send all data to channel api

--- bot/senderBotRouter.js
+++ bot/senderBotRouter.js
@@ -36,28 +36,22 @@ const sendMessageToBot = async (body) => {
     return new Promise((resolve, reject) => {
         let tBotUrl = `https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMediaGroup?chat_id=${process.env.CHANNEL_ID}`
         try {
-            resolve(request.post({
+            request.post({
                 url: tBotUrl,
                 json: true, // very important
                 body: body
             }, function (error, response, body) {
                 if (!error && response.statusCode == 200) {
                     // console.log("body ", body);
-                    return res.status(200).json({
-                        data: body
-                    })
+                    resolve(body)
                 } else if (error) {
                     console.log("error ", error)
-                    return res.status(400).json({
-                        "error": error
-                    })
+                    reject(error)
                 } else {
                     console.log("response ", response.body)
-                    return res.status(response.statusCode).json(
-                        response.body
-                    )
+                    resolve(body)
                 }
-            }))
+            })
         } catch (error) {
             reject(error)
         }
@@ -80,7 +74,7 @@ router.post("/send-message-from-db", async (req, res) => {
                 }
                 obj.media.push(imageObj)
             }
-            await sendMessageToBot(obj).catch(error=>console.log(error))
+            await sendMessageToBot(obj).catch(error => console.log(error))
         }
 
     }).catch(err => {
