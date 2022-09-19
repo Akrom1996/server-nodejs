693497148abdba4dcc994caa09ca663696370ce8
Akromjon <33409354+Akrom1996@users.noreply.github.com>
2022-09-19 12:23:36 +0500

    Update senderBotRouter.js

--- bot/senderBotRouter.js
+++ bot/senderBotRouter.js
@@ -1,5 +1,5 @@
 const express = require("express");
 const router = express.Router()
-
+const {sendMessageFromDB} = require("./senderBotController")
 router.post("/send-message-from-db",sendMessageFromDB)
 module.exports = router;
