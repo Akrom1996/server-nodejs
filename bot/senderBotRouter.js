0b0e74af62430981d81c6842bf3ed356eb8825ed
Akromjon <33409354+Akrom1996@users.noreply.github.com>
2022-09-19 10:20:12 +0500

    Update senderBotRouter.js

--- bot/senderBotRouter.js
+++ bot/senderBotRouter.js
@@ -65,17 +65,18 @@ router.post("/send-message-from-db", async (req, res) => {
             let caption = `#${result[j].title.replace(" ", "")} #${result[j].location}\n\n${result[j].description}\n${result[j].price}\n\nBarcha turdagi e'lonlaringizni tez va bepul joylashda 'Mandarin market' ilovasidan foydalaning.\nIlova uchun 👉 https://mandarinmarket.page.link/NEAo\n Kanalga ulanish uchun 👉 https://t.me/+gN5bCUJUHWZhYzA9`
             let obj = {}
             obj.media = []
-            for (let i = 0; i < result[j].images.length; i++) { //
-                let imageObj
-                if (i == 0) {
-                    imageObj = Object.fromEntries(Object.entries(new BotImageObjFirst("photo", `http://mandarinstorage.ngrok.io/p2p-market${result[j].images[i]}`, caption)))
-                } else {
-                    imageObj = Object.fromEntries(Object.entries(new BotImageObjOther("photo", `http://mandarinstorage.ngrok.io/p2p-market${result[j].images[i]}`)))
+            setTimeout(async () =>{
+                for (let i = 0; i < result[j].images.length; i++) { //
+                    let imageObj
+                    if (i == 0) {
+                        imageObj = Object.fromEntries(Object.entries(new BotImageObjFirst("photo", `http://mandarinstorage.ngrok.io/p2p-market${result[j].images[i]}`, caption)))
+                    } else {
+                        imageObj = Object.fromEntries(Object.entries(new BotImageObjOther("photo", `http://mandarinstorage.ngrok.io/p2p-market${result[j].images[i]}`)))
+                    }
+                    obj.media.push(imageObj)
                 }
-                obj.media.push(imageObj)
-            }
-setTimeout(async () => await sendMessageToBot(obj).then((data) => counter++).catch(error => console.log(error)), 2000)
-
+                await sendMessageToBot(obj).then((data) => counter++).catch(error => console.log(error))
+            },2000)
         }
         if (counter === Math.floor(result.length * 0.1)) {
             return res.status(200).json(new SuccessResponse("0", "0", "Successfully send data to telegram"))
