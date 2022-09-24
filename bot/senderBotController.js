f35b9d7d8dd1bd456a3d336d3c7db73a6866dd21
Akrom <akobidov777@gmail.com>
2022-09-24 19:31:22 +0500

    phone number for telegram bot

--- bot/senderBotController.js
+++ bot/senderBotController.js
@@ -88,7 +88,7 @@ exports.sendMessageFromDB = async (req, res) => {
             const user = await User.findById(result[j].user)
             let isNegotiable = result[j].isNegotiable?"Kelishamiz":"Oxirgi narxi"
             let newPhoneNumber = user.phoneNumber.replace(/[^0-9.]/g, '')
-            let caption = `#${result[j].title} #${result[j].location}\n<b>${result[j].position!=="null"?result[j].position:""}</b>\n${result[j].description}\n<b>${result[j].price}</b>\t<b>${isNegotiable}</b>\n\nAloqa uchun: <a href='tel:+${newPhoneNumber}'>${newPhoneNumber}</a>\n\nBarcha turdagi e'lonlaringizni <b>tez</b> va <b>bepul</b> joylashda <a href='https://mandarinmarket.page.link/NEAo'>Mandarin market</a> ilovasidan foydalaning.\nKanalga ulanish uchun 👉 https://t.me/+gN5bCUJUHWZhYzA9`
+            let caption = `#${result[j].title} #${result[j].location}\n<b>${result[j].position!=="null"?result[j].position:""}</b>\n${result[j].description}\n<b>${result[j].price}</b>\t<b>${isNegotiable}</b>\n\nAloqa uchun: <a href='tel:+${newPhoneNumber}'>+${newPhoneNumber}</a>\n\nBarcha turdagi e'lonlaringizni <b>tez</b> va <b>bepul</b> joylashda <a href='https://mandarinmarket.page.link/NEAo'>Mandarin market</a> ilovasidan foydalaning.\nKanalga ulanish uchun 👉 https://t.me/+gN5bCUJUHWZhYzA9`
             let obj = {}
             obj.media = []
             for (let i = 0; i < result[j].images.length; i++) { //
@@ -128,8 +128,8 @@ exports.sendItemToBot = async (result, userId) => {
     const user = await User.findById(userId)
     let isNegotiable = result.isNegotiable?"Kelishamiz":"Oxirgi narxi"
     let newPhoneNumber = user.phoneNumber.replace(/[^0-9.]/g, '')
-    console.log(`Aloqa uchun: <a href='tel:+${newPhoneNumber}'>${newPhoneNumber}</a>`)
-    let caption = `#${result.title} #${result.location}\n<b>${result.position!=="null"?result.position:""}</b>\n${result.description}\n<b>${result.price}</b>\t<b>${isNegotiable}</b>\n\nAloqa uchun: <a href='tel:+${newPhoneNumber}'>${newPhoneNumber}</a>\n\nBarcha turdagi e'lonlaringizni <b>tez</b> va <b>bepul</b> joylashda <a href='https://mandarinmarket.page.link/NEAo'>Mandarin market</a> ilovasidan foydalaning.\nKanalga ulanish uchun 👉 https://t.me/+gN5bCUJUHWZhYzA9`
+    console.log(`Aloqa uchun: <a href='tel:+${newPhoneNumber}'>+${newPhoneNumber}</a>`)
+    let caption = `#${result.title} #${result.location}\n<b>${result.position!=="null"?result.position:""}</b>\n${result.description}\n<b>${result.price}</b>\t<b>${isNegotiable}</b>\n\nAloqa uchun: <a href='tel:+${newPhoneNumber}'>+${newPhoneNumber}</a>\n\nBarcha turdagi e'lonlaringizni <b>tez</b> va <b>bepul</b> joylashda <a href='https://mandarinmarket.page.link/NEAo'>Mandarin market</a> ilovasidan foydalaning.\nKanalga ulanish uchun 👉 https://t.me/+gN5bCUJUHWZhYzA9`
     let obj = {}
     obj.media = []
     for (let i = 0; i < result.images.length; i++) { //
